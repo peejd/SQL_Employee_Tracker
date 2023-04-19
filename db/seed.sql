a10ef2008e2ab8acadb4101c55c68141d80ca92a
peejd <121059860+peejd@users.noreply.github.com>
2023-04-19 14:27:54 -0400

    added schema.sql and seed.sql files to the db folder

---
+++ db/seed.sql
@@ -0,0 +1,27 @@
+INSERT INTO department (name)
+VALUES 	("Executive"),
+		("Human Resources"),
+        ("Finance"),
+		("Sales"),
+		("Implementation"),
+		("Development"),
+		("Support");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", 1),
+       ("CFO", 1),
+       ("COO", 1),
+       ("HR Manager", 2),
+       ("Talent Recuiter", 2),
+       ("Development Director", 6),
+       ("Developer", 6),
+       ("Implementation Director", 5),
+       ("Implementation Engineer", 5),
+       ("Accountant", 3),
+       ("Sales Manager", 4),
+       ("Salesperson", 4),
+       ("Support Manager", 7),
+       ("Support Representative", 7),
+       
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Intro to JavaScript", 1),
